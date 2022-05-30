dd41cbd6ba81dd97fc3fb8b248479867ddad30eb
Guilherme Morone Araujo <guimorone@gmail.com>
2022-05-30 01:03:47 -0300

    pequenas mudanças

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,14 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-const footer = $(".footer-container");
+const footer = $('.footer-container');
 footer.before(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -19,12 +19,12 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 animate();
