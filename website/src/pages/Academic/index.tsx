9560380538f0c5db7867bc7d10378cae5b80c887
Guilherme Morone <guimorone@gmail.com>
2024-02-29 00:13:50 -0300

    Academic info att

--- website/src/pages/Academic/index.tsx
+++ website/src/pages/Academic/index.tsx
@@ -10,10 +10,18 @@ export default function Academic() {
 
 	const experiences = [
 		{
-			title: 'Student Teaching Assistant',
-			subtitle: 'Federal University of Pernambuco. Jun 2022 - Present',
+			title: 'Bachelor in Computer Engineering',
+			subtitle: 'Federal University of Pernambuco. Feb 2019 - Present',
 			description:
-				'Dedicated Student Teaching Assistant in the field of Software and Systems Engineering. Providing invaluable support to students in software development and requirements engineering, with a primary goal of crafting top-tier applications of exceptional quality.',
+				'I am engaged in the study of designing systems that integrate computer functionality with control systems, communications, and hardware components.',
+			comments: ['Expect to graduate by August 2024.', 'GPA: 9.07/10'],
+			Icon: { Element: GraduationCap, background: 'bg-rose-600' },
+		},
+		{
+			title: 'Student Teaching Assistant',
+			subtitle: 'Federal University of Pernambuco. Jun 2022 - Mar 2024',
+			description:
+				'Dedicated Student Teaching Assistant in the field of Software and Systems Engineering. Provided invaluable support to students in software development and requirements engineering, with a primary goal of crafting top-tier applications of exceptional quality.',
 			comments: [
 				'Skills/Technologies: ReactJS · Redux · NodeJS · Python · HTML · CSS · Unit Tests · Jest · Supertest · Enzyme · TypeScript · Web Development · Responsive Web Design · Software Development · SASS · Programming · REST APIs · Front-End Development · React Hooks and more.',
 			],
@@ -25,14 +33,6 @@ export default function Academic() {
 				},
 			],
 			Icon: { Element: ChalkboardUser, background: 'bg-rose-600' },
-		},
-		{
-			title: 'Bachelor in Computer Engineering',
-			subtitle: 'Federal University of Pernambuco. Feb 2019 - Present',
-			description:
-				'I am engaged in the study of designing systems that integrate computer functionality with control systems, communications, and hardware components.',
-			comments: ['Expect to graduate by August 2024.', 'GPA: 9.07/10'],
-			Icon: { Element: GraduationCap, background: 'bg-rose-600' },
 		},
 	];
 
