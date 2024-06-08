de98abbd381834cfde4db980435099207bfe1515
Guilherme <guimorone@gmail.com>
2024-06-08 01:11:02 -0300

    footer classname fixed

--- website/src/components/Banner/index.tsx
+++ website/src/components/Banner/index.tsx
@@ -27,7 +27,7 @@ const Banner: FC<IBannerProps> = ({ title = 'Hey, right here!', link }) => {
 				<div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-zinc-100 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
 					<p className="text-sm leading-6 text-zinc-900 hover:text-zinc-700">
 						<a href={link.url} target="_blank">
-							<strong className="font-semibold">{title}</strong>
+							<span className="font-semibold">{title}</span>
 							<svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
 								<circle cx={1} cy={1} r={1} />
 							</svg>
--- website/src/components/Footer/index.tsx
+++ website/src/components/Footer/index.tsx
@@ -4,29 +4,29 @@ import { InstagramIcon, GitHubIcon, LinkedInIcon } from '../../utils/icons';
 import { INSTAGRAM, GITHUB, LINKEDIN, WEBSITE_CODE_URL, MAILTO_URL } from '../../constants/urls';
 
 const navigation = [
-	{ name: 'Instagram', href: INSTAGRAM, target: '_blank', icon: InstagramIcon },
-	{ name: 'GitHub', href: GITHUB, target: '_blank', icon: GitHubIcon },
-	{ name: 'LinkedIn', href: LINKEDIN, target: '_blank', icon: LinkedInIcon },
-	{ name: 'E-mail', href: MAILTO_URL, icon: EnvelopeIcon },
-	{ name: 'Source code', href: WEBSITE_CODE_URL, target: '_blank', icon: CodeBracketIcon },
+	{ name: 'Instagram', href: INSTAGRAM, target: '_blank', Icon: InstagramIcon },
+	{ name: 'GitHub', href: GITHUB, target: '_blank', Icon: GitHubIcon },
+	{ name: 'LinkedIn', href: LINKEDIN, target: '_blank', Icon: LinkedInIcon },
+	{ name: 'E-mail', href: MAILTO_URL, Icon: EnvelopeIcon },
+	{ name: 'Source code', href: WEBSITE_CODE_URL, target: '_blank', Icon: CodeBracketIcon },
 ];
 
 export default function Footer() {
 	return (
-		<footer className="npmclear-both w-full sm:z-auto mt-auto max-w-7xl mx-auto">
+		<footer className="clear-both w-full sm:z-auto mt-auto max-w-7xl mx-auto">
 			<h2 className="sr-only">Social Medias</h2>
 			<section className="px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
 				<div className="flex justify-center space-x-6 md:order-2">
-					{navigation.map((item, index) => (
+					{navigation.map(({ name, href, target, Icon }, index) => (
 						<a
-							key={`footer_item_${item.name}_${index}`}
-							href={item.href}
-							target={item.target}
+							key={`footer_item_${name}_${index}`}
+							href={href}
+							target={target}
 							className="text-zinc-200 hover:cursor-pointer hover:text-zinc-400"
 						>
-							<Tooltip content={item.name} animation="duration-150">
-								<span className="sr-only">{item.name}</span>
-								<item.icon className="h-6 w-6" aria-hidden="true" />
+							<Tooltip content={name} animation="duration-150">
+								<span className="sr-only">{name}</span>
+								<Icon className="h-6 w-6" aria-hidden="true" />
 							</Tooltip>
 						</a>
 					))}
