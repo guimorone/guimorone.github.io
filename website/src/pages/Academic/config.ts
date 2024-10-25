c5709c63ca621c79c5e36982c7ba958ced614d9d
Guilherme <guimorone@gmail.com>
2024-10-24 21:23:40 -0300

    fixed where the final paper link should be

--- website/src/pages/Academic/config.ts
+++ website/src/pages/Academic/config.ts
@@ -13,6 +13,7 @@ export const experiences: ActivityType[] = [
 		subtitle: 'Federal University of Pernambuco. Feb 2019 - Aug 2024',
 		comments: ['Final grade (GPA): 9.01/10.0'],
 		Icon: { Element: GraduationCap, background: 'bg-rose-600' },
+		links: [{ Icon: GiNotebook, label: 'Undergraduate Thesis (PT-BR)', url: TCC_ATTENA_URL }],
 	},
 	{
 		title: 'Student Teaching Assistant',
@@ -25,6 +26,5 @@ export const experiences: ActivityType[] = [
 			'Skills/Technologies: ReactJS · Redux · NodeJS · Python · HTML · CSS · Unit Tests · Jest · Supertest · Enzyme · TypeScript · Web Development · Responsive Web Design · Software Development · SASS · Programming · REST APIs · Front-End Development · React Hooks and more.',
 		],
 		Icon: { Element: ChalkboardUser, background: 'bg-rose-600' },
-		links: [{ Icon: GiNotebook, label: 'Undergraduate Thesis (PT-BR)', url: TCC_ATTENA_URL }],
 	},
 ];
