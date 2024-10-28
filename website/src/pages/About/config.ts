058575f30014a0244a77cc240c8834f84b6cc977
Guilherme <guimorone@gmail.com>
2024-10-28 20:50:45 -0300

    updated about me text

--- website/src/pages/About/config.ts
+++ website/src/pages/About/config.ts
@@ -1,5 +1,5 @@
 export const title: string = "Hello, I'm Guilherme, and here is a nutshell version of who I am.";
 export const descriptions: string[] = [
 	"Originally from Rio but now based in Recife, I've had the privilege of growing up in two vibrant cities. My passions are as diverse as my background, ranging from an avid love for games and books to an insatiable curiosity for travel and technology. Furthermore, I am always eager to explore new opportunities and challenges that can help me grow both as a professional and as a person, fueled by my passion for learning and applying new technologies to solve real-world problems.",
-	'I have been working on Software Development since 2022. Not only that, but I am used to developing Big Data applications that demand high efficiency, scalability, and low cost. All my projects demanded skills in web development, data engineering, CI/CD, Docker, GitHub Actions, and cloud computing, in order to deliver quality solutions to clients.',
+	'I have been working on Software Development since 2022, and I am used to developing Big Data applications that demand high efficiency, scalability, and low cost. All my projects demanded skills in web development, data engineering, CI/CD, Docker, GitHub Actions, and cloud computing, in order to deliver quality solutions to clients.',
 ];
