7779cfb727e5fbeafbc5b753c6d2a39db8a8ca7c
Guilherme Morone Araujo <guimorone@gmail.com>
2025-04-09 00:00:31 -0300

    updated CV to new format with different sections

--- website/src/pages/Home/config.ts
+++ website/src/pages/Home/config.ts
@@ -1,12 +1,12 @@
 import { PDFIcon } from '../../utils/icons';
 import { formatDate } from '../../utils/dates';
 import { LAST_UPDATE_CV } from '../../constants/dates';
-import CV from '../../assets/pdfs/CV-Guilherme.pdf';
+import CV from '../../assets/pdfs/CV - Guilherme.pdf';
 import type { IconType } from 'react-icons';
 
 export const title: string = 'If you just want my Résumé, here it is:';
 export const label: string = 'CV';
 export const footerText: string = `Last update at ${formatDate(LAST_UPDATE_CV)}`;
 export const filePath: string = CV;
-export const filename: string = 'CV-Guilherme.pdf';
+export const filename: string = 'CV - Guilherme.pdf';
 export const Icon: IconType = PDFIcon;
