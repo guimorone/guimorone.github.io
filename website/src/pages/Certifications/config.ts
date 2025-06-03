d7fc4876a4ba743a76eee1c08b8db61ddc389424
Guilherme Morone Araujo <guimorone@gmail.com>
2025-06-02 23:47:02 -0300

    Reorganised Certifications config

--- website/src/pages/Certifications/config.ts
+++ website/src/pages/Certifications/config.ts
@@ -36,135 +36,84 @@ import type { ActivityType } from '../../@types';
 export const title: string = 'Courses & Certifications';
 export const subtitle: string = 'Allow me to showcase my certifications on different top-rated online platforms.';
 
+const UDEMY_ICON = { Element: SiUdemy, background: 'bg-[#A632F1]' };
+const GOOGLE_CLOUD_ICON = { Element: SiGooglecloud, background: 'bg-blue-600' };
+
+const getLinks = (url: string): ActivityType['links'] => [
+	{
+		Icon: GrCertificate,
+		color: 'text-teal-300 hover:text-teal-200',
+		label: 'Credential URL',
+		url,
+	},
+];
+
 export const coursesAndCertifications: ActivityType[] = [
 	{
 		title: 'Go - The Complete Guide',
 		subtitle: 'Udemy. Jun 2025',
 		skills: [GIT, GITHUB, GO],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: UDEMY_GO_COURSE_URL,
-			},
-		],
-		Icon: { Element: SiUdemy, background: 'bg-[#A632F1]' },
+		links: getLinks(UDEMY_GO_COURSE_URL),
+		Icon: UDEMY_ICON,
 	},
 	{
 		title: 'GraphQL by Example',
 		subtitle: 'Udemy. Nov 2024',
 		skills: [GIT, GITHUB, GRAPHQL, NODE, REACT, JAVASCRIPT, TYPESCRIPT],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: UDEMY_GRAPHQL_COURSE_URL,
-			},
-		],
-		Icon: { Element: SiUdemy, background: 'bg-[#A632F1]' },
+		links: getLinks(UDEMY_GRAPHQL_COURSE_URL),
+		Icon: UDEMY_ICON,
 	},
 	{
 		title: 'The Complete Cyber Security Course - Volume 1: Hackers Exposed!',
 		subtitle: 'Udemy. Nov 2024',
 		skills: [CYBER_SECURITY, LINUX],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: UDEMY_CYBER_SECURITY_COURSE_1_URL,
-			},
-		],
-		Icon: { Element: SiUdemy, background: 'bg-[#A632F1]' },
+		links: getLinks(UDEMY_CYBER_SECURITY_COURSE_1_URL),
+		Icon: UDEMY_ICON,
 	},
 	{
 		title: 'The Complete Flutter Development Bootcamp with Dart',
 		subtitle: 'Udemy. Apr 2024',
 		skills: [DART, FLUTTER, GIT, GITHUB],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: UDEMY_FLUTTER_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiUdemy, background: 'bg-[#A632F1]' },
+		links: getLinks(UDEMY_FLUTTER_COURSE_CERT_URL),
+		Icon: UDEMY_ICON,
 	},
 	{
 		title: 'Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud - Locales',
 		subtitle: 'Google Cloud Skills Boost. Mar 2023',
 		description: 'Credential ID: 3311200',
 		skills: [GCP],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: GOOGLE_CLOUD_IA_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiGooglecloud, background: 'bg-blue-600' },
+		links: getLinks(GOOGLE_CLOUD_IA_COURSE_CERT_URL),
+		Icon: GOOGLE_CLOUD_ICON,
 	},
 	{
 		title: 'Google Cloud Computing Foundations: Infrastructure in Google Cloud - Locales',
 		subtitle: 'Google Cloud Skills Boost. Mar 2023',
 		description: 'Credential ID: 3303084',
 		skills: [GCP],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: GOOGLE_CLOUD_INFRA_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiGooglecloud, background: 'bg-blue-600' },
+		links: getLinks(GOOGLE_CLOUD_INFRA_COURSE_CERT_URL),
+		Icon: GOOGLE_CLOUD_ICON,
 	},
 	{
 		title: 'Google Cloud Computing Foundations: Networking & Security - Locales',
 		subtitle: 'Google Cloud Skills Boost. Mar 2023',
 		description: 'Credential ID: 3305980',
 		skills: [CYBER_SECURITY, GCP],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: GOOGLE_CLOUD_NETWORKING_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiGooglecloud, background: 'bg-blue-600' },
+		links: getLinks(GOOGLE_CLOUD_NETWORKING_COURSE_CERT_URL),
+		Icon: GOOGLE_CLOUD_ICON,
 	},
 	{
 		title: 'Google Cloud Computing Foundations: Cloud Computing Fundamentals - Locales',
 		subtitle: 'Google Cloud Skills Boost. Mar 2023',
 		description: 'Credential ID: 3302433',
 		skills: [GCP],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: GOOGLE_CLOUD_FUNDAMENTALS_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiGooglecloud, background: 'bg-blue-600' },
+		links: getLinks(GOOGLE_CLOUD_FUNDAMENTALS_COURSE_CERT_URL),
+		Icon: GOOGLE_CLOUD_ICON,
 	},
 	{
 		title: 'The Complete 2020 Web Development Bootcamp',
 		subtitle: 'Udemy. Apr 2020',
 		skills: [CSS, GIT, GITHUB, HTML, JAVASCRIPT, JQUERY, MONGODB, NODE, REACT, SQL, TYPESCRIPT],
-		links: [
-			{
-				Icon: GrCertificate,
-				color: 'text-teal-300 hover:text-teal-200',
-				label: 'Credential URL',
-				url: UDEMY_WEB_DEV_COURSE_CERT_URL,
-			},
-		],
-		Icon: { Element: SiUdemy, background: 'bg-[#A632F1]' },
+		links: getLinks(UDEMY_WEB_DEV_COURSE_CERT_URL),
+		Icon: UDEMY_ICON,
 	},
 ];
