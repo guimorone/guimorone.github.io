3132b09bca9d105e63fd4561ceed68340ecfd0ae
Guilherme Morone Araujo <guimorone@gmail.com>
2025-11-03 13:02:02 -0300

    fixed about text

--- website/src/pages/About/config.ts
+++ website/src/pages/About/config.ts
@@ -1,7 +1,7 @@
 export const title: string = "Hello, I'm Guilherme, and here is a nutshell version of who I am";
 export const descriptions: string[] = [
 	'I am a Software Engineer with a background in Computer Engineering and a strong interest in building reliable, efficient, and scalable systems.',
-	'Since 2019, I have worked on a range of projects involving web development, data engineering, and cloud infrastructure — often with a focus on Big Data applications. I am comfortable working with tools like Python, Django, FastAPI, React, Docker, cloud platforms like AWS and GCP, and more.',
+	'Since 2019, I have worked on a range of projects involving web development, data engineering, and cloud infrastructure, often with a focus on Big Data applications. I am comfortable working with tools like Python, Django, FastAPI, React, Docker, cloud platforms like AWS and GCP, and more.',
 	'I enjoy solving real-world problems with code, collaborating with teams, and continuously learning new technologies.',
 	'Originally from Rio and now based in Recife, I bring curiosity, adaptability, and a passion for turning ideas into practical solutions.',
 ];
